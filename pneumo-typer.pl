8a13d98debf2124ab88353e43e4746de6cfe59ad
Xiangyang1984 <58356520+Xiangyang1984@users.noreply.github.com>
2023-10-06 11:28:21 +0800

    Update pneumo-typer.pl

--- pneumo-typer.pl
+++ pneumo-typer.pl
@@ -25,7 +25,7 @@ pneumo-typer.pl -d genbank_file_directory [options]
 
 FOR EXAMPLE: 
 
-perl [absolute path to] pneumo-typer.pl -d Test_data -t 10  -p T -srf /Users/zilinyang/Desktop/桌面材料/李向阳-论文投稿汇总/Serotype_ST_manuscript/bioconda/pneumo-typer-v1.0.1/order_sFig.txt -e /Users/zilinyang/Desktop/桌面材料/李向阳-论文投稿汇总/Serotype_ST_manuscript/bioconda/pneumo-typer-v1.0.1/expored_list.txt
+perl pneumo-typer.pl -d Test_data -t 10  -p T
 
 
 #######################################################################################################################################
@@ -39,24 +39,32 @@ perl [absolute path to] pneumo-typer.pl -d Test_data -t 10  -p T -srf /Users/zil
     ~~~~~~~~~~~~~~~~~~~
        -o, --output_directory
            An output directory holding all the generated files by pneumo-typer.pl. if this option is not set,  a directory named "ipneumo-pyper_workplace" will be created in the bin directory from where pneumo-typer.pl was invoked.
-       -m, --multiple_threads
+       -t, --multiple_threads
            Set thread number (Default: 1)
-       -jb, --start_at_blast 
-           Jump to a local blastn analysis, and Skips sequencing extraction (Default: F).  
+       -Ss, --skip_sequence_processing 
+           Skip the process of sequence processing (STEP-1) (Default: F).  
        -hgc, --homologous_gene_cutoff
            Set E-value, Identify, Coverage (Query and Subject), Match_length (alignment length) cutoff in Blastn analysis (default: E-value=1e-5, Identify=70, Coverage=95, Match_length=100).
-       -js, --jump_serotype
-           After blastn analysis, users can jump the process of doing blastn file to perform Serotype analysis again.
+       -Sb, --skip_blastn
+           Skip the process of doing blastn during serotype analysis.
        -p, --prodigal_annotation
            Annotate all genomes using prodigal. 
        -m, --mlst
            Perform mlst analysis (Default: T). 
        -c, --cgmlst
            Perform cgmlst analysis. It need >10 mins for one genome (Default: F).
+       -Rh, --recreate_heatmap                             
+           Re-create the heatmap of cps gene distribution in genomes (Default: F). At this step, users can add a parameter "phylogenetic_tree" or "strain_reorder_file". 
+       -Rf, --recreate_figure
+           Re-create the figure of the genetic organlization of cps gene cluster for genomes (Default: F). At this step, users can add a parameter "phylogenetic_tree" or "strain_reorder_file".
+       -tree, --phylogenetic_tree
+           A Newick format tree file is used by Pneumo-Typer to automatically accociate the genomes with their phylogeny. Meanwhile, Pneumo-Typer will output a file named "temp_strain_reorder_file", which contains the order information of genomes in tree from up to down. It should be noted that all nodes name in provided tree must completely match with the genbank files name of all genomes.
+       -srf, --strain_reorder_file
+           A two-column tab-delimited text file is used to sort genomes from up to down accoding to users requirement. Each row must consist of a strain name followed by the numerical order that is used for sorting genomes. It should be noted that all strains name must completely match with the genbank files name of all genomes.
        -Ts, --test
-           Run pneumo-typer using Test_data as input to check whether pneumo-typer is installed successfully (Default: F).
+           Run pneumo-typer using Test_data as input to check whether Pneumo-Typer is installed successfully (Default: F).
        -V, --version
-           The version of pneumo-typer.
+           The version of Pneumo-Typer.
        -h, --help
            Show this message.
 
@@ -75,12 +83,16 @@ my %options = (
     'genbank_file_directory'                       => undef,   
     'workplace_directory'                          => undef,  
     'multiple_threads'                             => "3",
-    'start_at_blast'                               => "F",
+    'skip_sequence_processing'                     => "F",
     'homologous_gene_cutoff'                       => "1e-5,70,95,100", #array to set blast parse cutoff: E-value, Identify, Coverage, Match_length
-    'jump_serotype'                                => "F",
+    'skip_blastn'                                  => "F",
     'prodigal_annotation'                          => "F",
     'mlst'                                         => "T",
     'cgmlst'                                       => "F",
+    'recreate_heatmap'                             => "F",
+    'recreate_figure'                              => "F",
+    'phylogenetic_tree'                            => undef,
+    'strain_reorder_file'                          => undef,
     'test'                                         => "F",
     'version'                                      => undef,
     'help'                                         => undef
@@ -92,12 +104,16 @@ GetOptions(
     'd|genbank_file_directory=s'                     => \$options{genbank_file_directory},    
     'o|workplace_directory=s'                        => \$options{workplace_directory},
     't|multiple_threads=i'                           => \$options{multiple_threads}, 
-    'jb|start_at_blast=s'                            => \$options{start_at_blast},       
+    'Ss|skip_sequence_processing=s'                  => \$options{skip_sequence_processing},       
     'hgc|homologous_gene_cutoff=s'                   => \$options{homologous_gene_cutoff}, 
-    'js|jump_serotype=s'                             => \$options{jump_serotype},
+    'Sb|skip_blastn=s'                               => \$options{skip_blastn},
     'p|prodigal_annotation=s'                        => \$options{prodigal_annotation},
     'm|mlst=s'                                       => \$options{mlst},
     'c|cgmlst=s'                                     => \$options{cgmlst},
+    'Rh|recreate_heatmap=s'                          => \$options{recreate_heatmap},
+    'Rf|recreate_figure=s'                           => \$options{recreate_figure},
+    'tree|phylogenetic_tree=s'                       => \$options{phylogenetic_tree},
+    'srf|strain_reorder_file=s'                      => \$options{strain_reorder_file},
     'Ts|test=s'                                      => \$options{test},
     'V|version'                                      => \$options{version},
     'h|help'                                         => \$options{help}
@@ -169,10 +185,14 @@ mkdir $path_gene;
 my $directory_TFT = $workplace."/Whole_TFT"; 
 mkdir $directory_TFT;
 
-my $map_cmd = $workplace."/map_cmd.txt"; 
-open (MAP_CMD, ">$map_cmd");
+$options{skip_sequence_processing} = "F" if $options{skip_blastn} eq "F";
 
-if ($options{start_at_blast} eq "F"){
+my $map_cmd = $workplace."/map_cmd.txt"; #store command to creat heatmap and figure
+
+if ( ($options{recreate_heatmap} eq "F")  && ($options{recreate_figure} eq "F") ){
+
+
+if ($options{skip_sequence_processing} eq "F"){
     print "STEP-1: Dealing with genomes extract genome sequence, gene sequences and gene feature table (TFT); annotate genome which has no annotation information using prodigal>\n";
 
     &PTyper::batch_genomenucleotide_extract_run ($path_genbank, $path_fa1, $path_fa2, $thread_number);
@@ -183,7 +203,7 @@ if ($options{start_at_blast} eq "F"){
 }
 
 
-print "\nSTEP-2: Determining the Sequence Type (ST)\n" if ( ($options{mlst} eq "T") or ($options{cgmlst} eq "T") );
+print "\nSTEP-2: Determining the Sequence Type (ST/cgST)\n" if ( ($options{mlst} eq "T") or ($options{cgmlst} eq "T") );
 
 if ($options{mlst} eq "T") {
     print "  \nSTEP-2.1: MLST analysis\n";
@@ -203,9 +223,9 @@ my $best_output = "$workplace/BEST.BLASTOUT";
 my $blastn_out_dir = "$workplace/blastn_out_dir";
 mkdir $blastn_out_dir;
 
-&PTyper::bacth_blast_best_run ($workplace, $path_gene, $blastn_out_dir, $db_file, $e_value, $blastn, $makeblastdb, $thread_number) if $options{jump_serotype} eq "F";
+&PTyper::bacth_blast_best_run ($workplace, $path_gene, $blastn_out_dir, $db_file, $e_value, $blastn, $makeblastdb, $thread_number) if $options{skip_blastn} eq "F";
 chdir $blastn_out_dir;
-system ("find . -type f -name '*.blastout' | xargs cat > $best_output");  # For large number of files, directly using cat may causing error
+system ("find . -type f -name '*.blastout' | xargs cat > $best_output") if $options{skip_blastn} eq "F";  # For large number of files, directly using cat may causing error
 
 my $all_vs_all_cluster = PTyper::blast_filter ($best_output, $e_value, $identify, $coverage, $match_length);
 
@@ -232,11 +252,13 @@ print "\nSTEP-4: Output sequence type and serotype results\n";
 
 system ("perl $home_directory/script/ser_join_ST.pl $workplace/Serotype.out $workplace/ST_out.txt $workplace/cgST_out.txt $workplace/Serotype_ST.out");
 
-#system ("rm -rf $new_db_file*"); #delete all serotype database files
 
 
 ### Visualizing the result
 ##########################
+
+
+open (MAP_CMD, ">$map_cmd");
 
 print "\nSTEP-5: Heatmaping the cps gene distribution in genomes\n";
 my $cmd_1 ="perl $home_directory/script/heatmap.pl -dir $workplace/result_statistics/tbl_heatmap_class -left 20 -scale 4 -label T -dis 9 -w 4 -l 0 -right 50 -cf $workplace/result_statistics/Statistics_OUT/classification_CPS -e $workplace/Serotype_ST.out -o $workplace";
@@ -245,7 +267,7 @@ system ("$cmd_1");
 my $cmd_2 ="perl $home_directory/script/heatmap.pl -dir $workplace/result_statistics/tbl_heatmap_gene -left 20 -scale 4 -label T -dis 9 -w 4 -l 0 -right 50 -cf $workplace/result_statistics/Statistics_OUT/classification_gene -e $workplace/Serotype_ST.out -o $workplace";
 system ("$cmd_2"); 
 
-print "\nSTEP-6: Visualizing the cps gene cluster in each genome\n";
+print "\nSTEP-6: Visualizing the cps gene cluster for genomes\n";
 
 my $gcluster_workplace = "$workplace/cps_cluster_workplace";
 mkdir $gcluster_workplace;
@@ -265,6 +287,30 @@ system ("$cmd_3");
 
 print MAP_CMD "heatmap_class:\n$cmd_1\n\nheatmap_gene:\n$cmd_2\n\ncps gene cluster:\n$cmd_3\n";
 close MAP_CMD;
+
+}
+
+
+my ($remap_cmd1, $remap_cmd2, $remap_cmd3) = &PTyper::obtain_map_cmd ($map_cmd, \%options);
+
+#remap heatmap
+if ($options{recreate_heatmap} eq "T"){
+
+    print "\nRe-create the heatmap of cps gene distribution in genomes (STEP-5)\n\n";
+    system ("$remap_cmd1");
+    system ("$remap_cmd2");
+
+}
+
+#remap figure
+if ($options{recreate_figure} eq "T"){
+
+    print "\nRe-create the figure of the genetic organlization of cps gene cluster for genomes (STEP-6)\n\n";
+    system ("$remap_cmd3");
+
+}
+
+
 
 my $finish_time = localtime;
 print "\n$finish_time: done!\n\n";
